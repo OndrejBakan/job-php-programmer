e1715997e3b565cebfcd0117cafaed0d7349e13c
Ondrej Bakan <ondrej@bakan.cz>
2022-10-05 18:52:29 +0200

    WIP tests.

---
+++ tests/Feature/CustomerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Customer;
+use App\Models\CustomerGroup;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class CustomerTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_specific_customer_should_not_include_customer_group_by_default()
+    {
+        $customer = Customer::factory(1)->create();
+        $customerGroup = CustomerGroup::factory(1)->create();
+
+        $rel = DB::table('customer_customer_group')->insert([
+            'customer_id'       => $customer->first->id,
+            'customer_group_id' => $customerGroup->first->id,
+            'created_at'        => now(),
+            'updated_at'        => now(),
+        ]);
+
+        $response = $this->getJson('/api/v1/customers/' . $customer->first->id);
+        $response->dump();
+        $response
+            ->assertJson(fn (AssertableJson $json) => 
+                $json
+                    ->where('id', $customer->first->id)
+                    ->where('name', $customer->first->name)
+                    ->etc()
+            );
+    }
+}
